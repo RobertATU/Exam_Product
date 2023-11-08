15deb463e7c25282a977fc9a1bfd2d61319dbf7a
G00395612@atu.ie <G00395612@atu.ie>
2023-11-08 09:09:51 +0000

    Initial commit

---
+++ src/main/java/ie/atu/exam_product/ExamProductApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.exam_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProductApplication.class, args);
+    }
+
+}
